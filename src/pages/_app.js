4921efa80f984eecb337f51b4a6e859ab9f05fea
mictlantecutli <par21078@byui.edu>
2024-12-20 15:13:38 -0600

    responsiveness

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,9 @@ import { Toaster } from "react-hot-toast";
 import { Layout } from "../components";
 import "../styles/globals.css";
 import "../styles/index.scss"; /* main styles */
-// import "../../dist/output.css";
+// import "../styles/mobile.css";
+// import "../styles/tablet.css";
+import "../styles/overrides/portable.css";
 
 import { StateContext } from "../../context/StateContext";
 
